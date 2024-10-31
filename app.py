c5e4a3bedee0fe4fd13cc4343c34c25c691ed615
Lucas Valdetario <lucas.valdetario@gmail.com>
2024-10-31 17:09:18 -0300

    Add files via upload
    Update files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+# __name__ = "__main__"
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello_world"
+
+@app.route("/about")
+def about():
+    return "Página sobre"
+
+if __name__ == "__main__":
+    app.run(debug=True)
